e00a7eef4f69b066d8ef6a21deabd685f1614c71
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-18 13:41:50 +0300

    Default Changelist index.html changed; timer.js changed;

--- js/module/timer.js
+++ js/module/timer.js
@@ -1,17 +1,17 @@
 export const handleTimer = () => {
-    const changeBannerPromoText = () => {
-        const timerPromoText = document.querySelector('.item__text-notebook');
-        timerPromoText.textContent = 'ноутбуки - всегда отличный подарок';
-        timerPromoText.classList.add('item__text-notebook-changed');
-        const itemGallery = timerPromoText.closest('.item__gallery-notebook');
-        itemGallery.classList.add('item__gallery-notebook-changed');
-    };
+  const changeBannerPromoText = () => {
+    const timerPromoText = document.querySelector('.item__text-notebook');
+    timerPromoText.textContent = 'ноутбуки - всегда отличный подарок';
+    timerPromoText.classList.add('item__text-notebook-changed');
+    const itemGallery = timerPromoText.closest('.item__gallery-notebook');
+    itemGallery.classList.add('item__gallery-notebook-changed');
+  };
 
-    const deadline = document.querySelector('.timer');
-    if (deadline) {
-        const hasDeadlineAttr = deadline.hasAttribute('data-timer-deadline');
-        if (hasDeadlineAttr) {
-            deadline.insertAdjacentHTML('beforeend', `
+  const deadline = document.querySelector('.timer');
+  if (deadline) {
+    const hasDeadlineAttr = deadline.hasAttribute('data-timer-deadline');
+    if (hasDeadlineAttr) {
+      deadline.insertAdjacentHTML('beforeend', `
             <p class="timer__title">До конца акции:</p>
             <div class="timer__block">
               <p><span class="timer__count timer-days-num">00&nbsp;
@@ -24,108 +24,117 @@ export const handleTimer = () => {
               </span><span class="timer__units  timer-seconds-text">секунд</span></p>
             </div>
           `);
-        }
-    } else {
-        changeBannerPromoText();
-        return;
     }
+  } else {
+    changeBannerPromoText();
+    return;
+  }
 
-    const deadlineAttr = deadline.getAttribute('data-timer-deadline');
-    const timerBlockDays = document.querySelector('.timer-days-num');
-    const timerBlockHour = document.querySelector('.timer-hours-num');
-    const timerBlockMinutes = document.querySelector('.timer-minutes-num');
-    const timerBlockSeconds = document.querySelector('.timer-seconds-num');
+  const deadlineAttr = deadline.getAttribute('data-timer-deadline');
+  const timerBlockDays = document.querySelector('.timer-days-num');
+  const timerBlockHour = document.querySelector('.timer-hours-num');
+  const timerBlockMinutes = document.querySelector('.timer-minutes-num');
+  const timerBlockSeconds = document.querySelector('.timer-seconds-num');
 
-    const textDays = document.querySelector('.timer-days-text');
-    const textHour = document.querySelector('.timer-hours-text');
-    const textMinutes = document.querySelector('.timer-minutes-text');
-    const textSeconds = document.querySelector('.timer-seconds-text');
+  const textDays = document.querySelector('.timer-days-text');
+  const textHour = document.querySelector('.timer-hours-text');
+  const textMinutes = document.querySelector('.timer-minutes-text');
+  const textSeconds = document.querySelector('.timer-seconds-text');
 
-    const getTimeRemaining = () => {
-        const dateStop = new Date(deadlineAttr).getTime();
-        const dateNow = Date.now();
+  const getTimeRemaining = () => {
+    const dateStop = new Date(deadlineAttr).getTime();
+    const dateNow = Date.now();
 
-        const timezoneoffset = Math.abs(new Date().getTimezoneOffset()) * 60 * 1000;
-        const myTimeZone = (+3) * 60 * 60 * 1000;
+    const timezoneoffset = Math.abs(new Date().getTimezoneOffset()) * 60 * 1000;
+    const myTimeZone = (+3) * 60 * 60 * 1000;
 
-        const myCurrentTime = dateNow - timezoneoffset + myTimeZone;
+    const myCurrentTime = dateNow - timezoneoffset + myTimeZone;
 
-        const timeRemaining = dateStop - myCurrentTime;
-        // const timeRemaining = dateStop - dateNow;
+    const timeRemaining = dateStop - myCurrentTime;
+    // const timeRemaining = dateStop - dateNow;
 
-        const seconds = Math.floor(timeRemaining / 1000 % 60);
-        const minutes = Math.floor(timeRemaining / 1000 / 60 % 60);
-        const hours = Math.floor(timeRemaining / 1000 / 60 / 60 % 24);
-        const days = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
+    const seconds = Math.floor(timeRemaining / 1000 % 60);
+    const minutes = Math.floor(timeRemaining / 1000 / 60 % 60);
+    const hours = Math.floor(timeRemaining / 1000 / 60 / 60 % 24);
+    const days = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
 
 
-        return {
-            timeRemaining,
-            seconds,
-            minutes,
-            hours,
-            days,
-        };
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+      timeRemaining,
     };
+  };
 
-    const handleTextDeclension =
-        ({days: day, seconds: sec, minutes: min, hours: hour}) => {
-            const days = [[0, 0, 'дней'], [1, 1, 'день'],
-                [2, 4, 'дня'], [5, 9, 'дней']];
+  const handleTextDeclension =
+    ({days: day, hours: hour, minutes: min, seconds: sec}) => {
 
-            const hours = [[0, 0, 'часов'], [1, 1, 'час'],
-                [2, 4, 'часа'], [5, 9, 'часов']];
-            const minutes = [[0, 0, 'минут'], [1, 1, 'минута'],
-                [2, 4, 'минуты'], [5, 9, 'минут']];
-            const seconds = [[0, 0, 'секунд'], [1, 1, 'секунда'],
-                [2, 4, 'секунды'], [5, 9, 'секунд']];
-            const exludeRange = [11, 19];
+      const timerArray = [day, hour, min, sec];
 
-            const currentDate = [[day, days, textDays], [hour, hours, textHour],
-                [min, minutes, textMinutes], [sec, seconds, textSeconds]];
+      const dayNumber = [[0], [1], [2, 4]];
+      const ofDays = 'дней-день-дня';
+      const ofHours = 'час-часов-часа';
+      const ofMinutes = 'минут-минута-минуты';
+      const ofSeconds = 'секунд-секунда-секунды';
 
+      const days = [[0, 0, 'дней'], [1, 1, 'день'],
+        [2, 4, 'дня'], [5, 9, 'дней']];
 
-            const getLastDigit = (number) => number % 10;
+      const hours = [[0, 0, 'часов'], [1, 1, 'час'],
+        [2, 4, 'часа'], [5, 9, 'часов']];
+      const minutes = [[0, 0, 'минут'], [1, 1, 'минута'],
+        [2, 4, 'минуты'], [5, 9, 'минут']];
+      const seconds = [[0, 0, 'секунд'], [1, 1, 'секунда'],
+        [2, 4, 'секунды'], [5, 9, 'секунд']];
+      const excludeRange = [11, 19];
 
-            const insideExludeRange = ([left, right], number) =>
-                number >= left && number <= right;
-
-            for (const [number, numAndText, textSelector] of currentDate) {
-                if (insideExludeRange(exludeRange, number)) return;
-                const digit = getLastDigit(number);
-                for (const [left, right, text] of numAndText) {
-                    if (digit >= left && digit <= right) {
-                        textSelector.textContent = text;
-                    }
-                }
-            }
-        };
-
-    const start = () => {
-        const timer = getTimeRemaining();
-        handleTextDeclension(timer);
+      const currentDate = [[day, days, textDays], [hour, hours, textHour],
+        [min, minutes, textMinutes], [sec, seconds, textSeconds]];
 
 
-        timer.days === 0 ? timerBlockDays.closest('p').remove() :
-            timerBlockDays.textContent = timer.days.toString();
-        timerBlockHour.textContent = timer.hours.toString();
-        timerBlockMinutes.textContent = timer.minutes.toString();
-        timerBlockSeconds.textContent = timer.seconds.toString();
+      const getLastDigit = (number) => number % 10;
 
-        const intevalId = setTimeout(start, 1000);
+      const insideExludeRange = ([left, right], number) =>
+        number >= left && number <= right;
 
-        if (timer.timeRemaining <= 0 || isNaN(timer.days)) {
-            timerBlockDays.textContent = '00';
-            timerBlockHour.textContent = '00';
-            timerBlockMinutes.textContent = '00';
-            timerBlockSeconds.textContent = '00';
-            clearTimeout(intevalId);
-            const timer = document.querySelector('.timer');
-            timer.remove();
-            changeBannerPromoText();
+      for (const [number, numAndText, textSelector] of currentDate) {
+        if (insideExludeRange(excludeRange, number)) return;
+        const digit = getLastDigit(number);
+        for (const [left, right, text] of numAndText) {
+          if (digit >= left && digit <= right) {
+            textSelector.textContent = text;
+          }
         }
+      }
     };
 
+  const start = () => {
+    const timer = getTimeRemaining();
+    handleTextDeclension(timer);
 
-    start();
+
+    timer.days === 0 ? timerBlockDays.closest('p').remove() :
+      timerBlockDays.textContent = timer.days.toString();
+    timerBlockHour.textContent = timer.hours.toString();
+    timerBlockMinutes.textContent = timer.minutes.toString();
+    timerBlockSeconds.textContent = timer.seconds.toString();
+
+    const intevalId = setTimeout(start, 1000);
+
+    if (timer.timeRemaining <= 0 || isNaN(timer.days)) {
+      timerBlockDays.textContent = '00';
+      timerBlockHour.textContent = '00';
+      timerBlockMinutes.textContent = '00';
+      timerBlockSeconds.textContent = '00';
+      clearTimeout(intevalId);
+      const timer = document.querySelector('.timer');
+      timer.remove();
+      changeBannerPromoText();
+    }
+  };
+
+
+  start();
 };
